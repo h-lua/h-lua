77ef9fb1d019d35a1aa3c2c838eac46810b5ad2d
hunzsig <mzyhaohaoren@qq.com>
2021-03-25 07:58:47 +0800

    chore(*): 更新工具

--- depend/h-lua/const/abilityTarget.lua
+++ depend/h-lua/const/abilityTarget.lua
@@ -1,5 +1,6 @@
 -- 技能冷却目标
 CONST_ABILITY_TARGET = {
+    none = { value = "none", label = "无目标" },
     location = { value = "location", label = "点目标" },
     range = { value = "range", label = "范围选择" },
     unit = { value = "unit", label = "单位目标" },
