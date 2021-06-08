5ea2b187725a90f7b3290a001e7ebb0743c7d4e0
xxqq <5382557@qq.com>
2021-06-08 12:47:23 +0800

    漂浮文字速率独立化

--- depend/h-lua/const/textTag.lua
+++ depend/h-lua/const/textTag.lua
@@ -1,8 +1,12 @@
 --- 全局漂浮字默认速率
 CONST_MODEL_TTG_SPD = 1.0
 --- 全局伤害系统漂浮字默认速率
-CONST_MODEL_TTG_SPD_DMG = 1.0
-
+CONST_MODEL_TTG_SPD_DMG = {
+    ordinary = 1.0, -- 普通
+    ignores = 1.2, -- 无视
+    Knocking = 1.4, -- 暴击
+    enchant = 1.6, -- 附魔
+}
 --- 模型贴图型特效
 CONST_MODEL_TTG = {
     ["k"] = { mdl = "hLua\\ttg\\l_K.mdl", bit = 2 },
